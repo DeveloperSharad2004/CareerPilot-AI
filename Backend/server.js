f8bae179a02fe6485e4a3d2d8feebc72401e25ad
Sharad Jaiswal <sharadjais80@gmail.com>
2026-07-12 12:44:55 +0530

    Update server configuration

--- Backend/server.js
+++ Backend/server.js
@@ -9,6 +9,6 @@ dns.setDefaultResultOrder("ipv4first");
 
 connectToDB();
 
-app.listen("3000", () =>{
-    console.log("Server is running on port 3000");
+app.listen(process.env.PORT, () =>{
+    console.log(`Server is running on port ${process.env.PORT}`);
 })
